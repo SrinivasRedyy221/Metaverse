50f4af313b533c0610dc82459a425f3405db43df
Srinivas Reddy <piserla.srinivas@gmail.com>
2023-08-16 12:41:11 -0700

    updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,15 +12,15 @@ const Footer = () => (
   >
     <div className='footer-gradient' />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-      <div className='flex items-center justify-between flex-wrap gap-5 '>
+      <div className='flex items-center justify-between flex-wrap gap-5'>
         <h4 className='font-bold md:text-[64px] text-[44px] text-white'>Enter the metaverse</h4>
-        <button type="button" className='flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px] '>
-          <img src="/headset.svg" alt="" className='w-[24px] h-[24px] object-contain' />
+        <button type='button' className='flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]'>
+          <img src='/headset.svg' alt='' className='w-[24px] h-[24px] object-contain' />
           <span className='font-normal text-[16px] text-white'>ENTER METAVERSE</span>
         </button>
       </div>
-      <div className='flex flex-col '>
-        <div className='mb-[50px] h-[2px] bg-white opacity-10' />
+      <div className='flex flex-col'>
+        <div className='mb-[50px] h-[2px] bg-white opacity-10'></div>
         <div className='flex items-center justify-between flex-wrap gap-4'>
           <h4 className='font-extrabold text-[24px] text-white'>Metaverses</h4>
           <p className='font-normal text-[14px] text-white opacity-50'>Copyright @ 2022 - 2023 Metaverses. All rights reserved.</p>
