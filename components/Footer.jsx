a6540bcbea7a1254bc46129873a0740dc86ed1aa
Srinivas Reddy <piserla.srinivas@gmail.com>
2023-08-16 12:56:28 -0700

    updateed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">Enter the metaverse</h4>
-        <button type='button' className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]">
-          <img src='/headset.svg' alt='' className="w-[24px] h-[24px] object-contain"/>
+        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]">
+          <img src="/headset.svg" alt="" className="w-[24px] h-[24px] object-contain" />
           <span className="font-normal text-[16px] text-white">ENTER METAVERSE</span>
         </button>
       </div>
@@ -26,7 +26,7 @@ const Footer = () => (
           <p className="font-normal text-[14px] text-white opacity-50">Copyright @ 2022 - 2023 Metaverses. All rights reserved.</p>
           <div className="flex gap-4">
             {socials.map((social) => (
-              <img key={social.name} src={social.url} alt={social.name} className='w-[24px] h-[24px] object-contain cursor-pointer' />
+              <img key={social.name} src={social.url} alt={social.name} className="w-[24px] h-[24px] object-contain cursor-pointer" />
             ))}
           </div>
         </div>
