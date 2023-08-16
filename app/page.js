b5d81851572215e6475f4f77a46eb15fde79dd39
Srinivas Reddy <piserla.srinivas@gmail.com>
2023-08-16 14:29:16 -0700

    updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
-
+import {useClient} from 'next/client'
+useClient()
 const Page = () => (
+  
   <div className='bg-primary-black overflow-hidden '>
     <Navbar />
     <Hero />
